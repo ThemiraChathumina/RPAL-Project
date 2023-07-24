468b2db6a4b9d8ebdbc8e9b9392233019dbdeda7
ThemiraChathumina <110608712+ThemiraChathumina@users.noreply.github.com>
2023-07-24 14:44:37 +0530

    added test case for cse

--- test5.cpp
+++ test5.cpp
@@ -1,10 +1,29 @@
 #include "st.h"
+#include "lexical_scanner.h"
+#include "ast.h"
 #include <iostream>
+
+Node *get_sample()
+{
+    Node *root = new Node("gamma", NAN);
+    root->left = new Node("Print", IDENTIFIER);
+    root->left->right = new Node("*", NAN);
+    root->left->right->left = new Node("gamma", NAN);
+    root->left->right->left->right = new Node("2", INTEGER);
+    root->left->right->left->left = new Node("lambda", NAN);
+    root->left->right->left->left->right = new Node("4", INTEGER);
+    root->left->right->left->left->left = new Node("x", IDENTIFIER);
+    root->left->right->left->left->left->right = new Node("-", NAN);
+    root->left->right->left->left->left->right->left = new Node("x", IDENTIFIER);
+    root->left->right->left->left->left->right->left->right = new Node("1", INTEGER);
+    return root;
+}
 
 int main()
 {
 
-    Node *root = getST("1.txt");
-    printAST(root); // fuck
+    // Node *root = getST("1.txt");
+    Node *root = get_sample();
+    printAST(root);
     return 0;
 }
