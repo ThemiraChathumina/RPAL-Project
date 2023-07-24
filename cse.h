8b4654371ad1147a3742f82fa1addc8d1ad9fcc7
ThemiraChathumina <110608712+ThemiraChathumina@users.noreply.github.com>
2023-07-24 15:03:30 +0530

    aaaa

--- cse.h
+++ cse.h
@@ -3,6 +3,15 @@
 
 #include "st.h"
 
+struct CSENode
+{
+    string value;
+    int next;
+    int current;
+    vector<string> var;
+    CSENode(string v, int n, int c, vector<string> v2) : value(v), next(n), current(c), var(v2) {}
+};
+
 vector<vector<Node>> getControlStruct(Node *root);
 void execute(vector<vector<Node>> contStruc);
 
