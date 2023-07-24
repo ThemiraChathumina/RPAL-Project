db991acf6953d6fcc33aad5001e219b6134ad277
ThemiraChathumina <110608712+ThemiraChathumina@users.noreply.github.com>
2023-07-24 16:05:12 +0530

    update cse.h

--- cse.h
+++ cse.h
@@ -3,16 +3,42 @@
 
 #include "st.h"
 
-struct CSENode
+enum Type
 {
-    string value;
-    int next;
-    int current;
-    vector<string> var;
-    CSENode(string v, int n, int c, vector<string> v2) : value(v), next(n), current(c), var(v2) {}
+    INT,
+    STRING,
+    BOOL,
+    OP,
+    TAU,
+    LAMBDA,
+    GAMMA,
+    DELTA,
+    ENV,
+    ID,
+    LIST
 };
 
-vector<vector<Node>> getControlStruct(Node *root);
-void execute(vector<vector<Node>> contStruc);
+struct ControlNode
+{
+    Type type;
+    string value;
+    int next;
+    vector<string> var;
+    ControlNode(Type t, string v, int c = NULL, vector<string> var = {}) : type(t), value(v), next(c), var(var) {}
+};
+
+struct StackNode
+{
+    Type type;
+    string value;
+    int current;
+    int next;
+    vector<string> var;
+    vector<StackNode> children;
+};
+
+vector<vector<ControlNode>>
+getControlStruct(Node *root);
+void execute(vector<vector<ControlNode>> contStruc);
 
 #endif
